02e0d410b7792d3a40bd8d02af8a5db155acc22a
madalynmm <madalynmm@outlook.com>
2022-11-15 22:23:57 -0500

    added questions for user prompts

--- Develop/index.js
+++ Develop/index.js
@@ -6,36 +6,52 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-    type: 'input',
-    name: 'location',
-    message: 'Where are you from?',
-    },
-    {
-    type: 'input',
-    name: 'hobby',
-    message: 'What is your favorite hobby?',
-    },
-    {
-    type: 'input',
-    name: 'food',
-    message: 'What is your favorite food?',
-    },
-    {
+  {
     type: 'input',
     name: 'github',
-    message: 'Enter your GitHub Username',
-    },
-    {
+    message: 'What is your GitHub username?',
+  },
+  {
     type: 'input',
-    name: 'linkedin',
-    message: 'Enter your LinkedIn URL.',
-    },
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project.',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'dependencies',
+    message: 'What command should be run to install dependencies?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What command should be run to run tests?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+  },
 ];
 
 // TODO: Create a function to write README file
