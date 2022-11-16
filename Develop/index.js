356c7ce387841ccb89338b109c70e009d5826737
madalynmm <madalynmm@outlook.com>
2022-11-15 21:57:03 -0500

    added functions for each user input and page links

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+    type: 'input',
+    name: 'location',
+    message: 'Where are you from?',
+    },
+    {
+    type: 'input',
+    name: 'hobby',
+    message: 'What is your favorite hobby?',
+    },
+    {
+    type: 'input',
+    name: 'food',
+    message: 'What is your favorite food?',
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username',
+    },
+    {
+    type: 'input',
+    name: 'linkedin',
+    message: 'Enter your LinkedIn URL.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,151 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// License badge, link, and section
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    
+    This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
+
+// Installation link and section
+function renderDependenciesLink(dependencies) {
+  if (dependencies !== 'None') {
+    return `\n* [Installation](#dependencies)\n`;
+  }
+  return '';
+}
+
+function renderDependenciesSection(dependencies) {
+  if (dependencies !== 'None') {
+    return `## Installation
+    
+    To install necessary dependencies, run the following command:
+
+    ${dependencies}
+    
+    `;
+  }
+  return '';
+}
+
+// Usage link and section
+function renderUsageLink(usage) {
+  if (usage !== 'None') {
+    return `\n* [Usage](#usage)\n`;
+  }
+  return '';
+}
+
+function renderUsageSection(usage) {
+  if (usage !== 'None') {
+    return `## Usage
+    
+    ${usage}
+    
+    `;
+  }
+  return '';
+}
+
+// Contributing link and section
+function renderContributingLink(contributing) {
+  if (contributing !== 'None') {
+    return `\n* [Contributing](#contributing)\n`;
+  }
+  return '';
+}
+
+function renderContributingSection(contributing) {
+  if (contributing !== 'None') {
+    return `## Contributing
+    
+    ${contributing}
+    
+    `;
+  }
+  return '';
+}
+
+// Tests link and section
+function renderTestsLink(tests) {
+  if (tests !== 'None') {
+    return `\n* [Tests](#tests)\n`;
+  }
+  return '';
+}
+
+function renderTestsSection(tests) {
+  if (tests !== 'None') {
+    return `## Tests
+
+    To run tests, run the following command:
+    
+    ${tests}
+    
+    `;
+  }
+  return '';
+}
+
+// Questions link and section
+function renderQuestionsLink() {
+  return `\n* [Questions](#questions)\n`;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ${renderDependenciesLink(data.dependencies)}
+
+  ${renderUsageLink(data.usage)}
+
+  ${renderLicenseLink(data.license)}
+
+  ${renderContributingLink(data.contributing)}
+
+  ${renderTestsLink(data.tests)}
+
+  ${renderQuestionsLink()}
+
+  
+  ${renderDependenciesSection(data.dependencies)}
+
+  ${renderUsageSection(data.usage)} 
+
+  ${renderLicenseSection(data.license)}
+
+  ${renderContributingSection(data.contributing)} 
+
+  ${renderTestsSection(data.tests)}
+
+  ## Questions
+    
+    If you have any questions about the repo, open an issue, or contact me directly at ${data.email}. You can find more of my work at ${renderGitHubLink(data.github)}.
 
 `;
 }
