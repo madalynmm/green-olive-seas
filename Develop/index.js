746054c9c2c94f556f9052e69bf84680d996b7e0
madalynmm <madalynmm@outlook.com>
2022-11-15 23:25:28 -0500

    updated index.js

--- Develop/index.js
+++ Develop/index.js
@@ -55,10 +55,24 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  
+  fs.writeFile(fileName, data, (err) => 
+    err ? console.log(err) : console.log('Successfully created README.md!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const data = generateMarkdown(answers); 
+      const fileName = path;
+      writeToFile(fileName, data);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -145,7 +145,7 @@ function generateMarkdown(data) {
 
   ## Questions
     
-    If you have any questions about the repo, open an issue, or contact me directly at ${data.email}. You can find more of my work at ${renderGitHubLink(data.github)}.
+    If you have any questions about the repo, open an issue, or contact me directly at ${data.email}. You can find more of my work at ${data.github}.
 
 `;
 }
